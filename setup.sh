eb720944b978d94cb223121a56d870bbf7d63a98
Luiz Augusto Rodrigues <61833154+Luizaugusto1962@users.noreply.github.com>
2024-08-12 15:38:47 -0300

    Setup.sh
    Ajuste de aparência

--- setup.sh
+++ setup.sh
@@ -10,7 +10,8 @@ echo "###      ( Parametros para serem usados no atualiza.sh )          ###" >> 
 echo "###      ( Parametros para serem usados no atualiza.sh )          ###" 
 echo $traco >> atualizac
 echo "###           ( USA BANCO (S/N) )                                 ###"
-read -rp "( USA BANCO (S/N)  ->" -n1 BANCO 
+echo $linha
+read -rp "      ( USA BANCO (S/N)  -> " -n1 BANCO 
 echo
 echo $linha
     if [[ "$BANCO" =~ ^[Nn]$ ]] || [[ "$BANCO" == "" ]]; then
@@ -18,16 +19,14 @@ echo $linha
     elif [[ "$CONT" =~ ^[Ss]$ ]]; then
         echo "BANCO=s" >> atualizac
     fi
-echo $linha >> atualizac
 declare -l DIR
 echo "###              ( PASTA DO SISTEMA )         ###" 
-read -rp "Informe o diretorio raiz ->" -n1 DIR 
-echo 
+read -rp "      Informe o diretorio raiz -> " DIR 
 echo destino="/$DIR" >> atualizac
 echo $linha 
 declare -l OFF
 echo "###          Tipo de acesso                  ###"
-read -rp "Servidor OFF S ou N ->" -n1 OFF 
+read -rp "Servidor em modo OFFLINE S ou N ->" -n1 OFF 
 echo
 if [[ "$OFF" =~ ^[Nn]$ ]] || [[ "$OFF" == "" ]]; then
         echo "#SERACESOFF=/sav/portalsav/Atualiza"  >> atualizac
@@ -38,21 +37,22 @@ echo $linha
 declare -l PASTA
 echo "###          ( Nome de pasta no servidor da SAV )                ###"
 echo "Nome de pasta no servidor da SAV, informar somento e pasta do cliente"
+echo $linha
 read -rp "/cliente/" PASTA 
-echo 
+echo $linha 
 if [[ "$PASTA" == "" ]]; then
     echo "ENVIABACK=/sav/portalsav/Atualiza"
     echo "ENVIABACK=/sav/portalsav/Atualiza" >> atualizac
 else
-    echo "ENVIABACK=cliente/""$PASTA"
-    echo "ENVIABACK=cliente/""$PASTA"  >> atualizac
+    echo "ENVIABACK=/cliente/""$PASTA"
+    echo "ENVIABACK=/cliente/""$PASTA"  >> atualizac
 fi
 echo $linha 
 declare -u EMPR
 echo "###           ( NOME DA EMPRESA )            ###"
 echo $linha
-read -rp "Nome da Empresa-> " EMPR 
-echo 
+read -rp "      Nome da Empresa-> " EMPR 
+echo $linha 
 echo EMPRESA="$EMPR"
 echo EMPRESA="$EMPR" >> atualizac
 echo $linha
@@ -61,32 +61,32 @@ echo $linha
 declare -l BASE
 declare -l BASE2
 declare -l BASE3
-read -rp "Nome de pasta da base, Ex: sav/dados_? -:>" BASE 
+read -rp "Nome de pasta da base de dados Ex: /sav/dados_? -:>" BASE 
 if [[ "$BASE" == "" ]]; then
 echo "Necessario pasta informar a base de dados" 
 exit
 else
-echo "base=/""$BASE"
-echo "base=/""$BASE" >> atualizac
+echo "base=""$BASE"
+echo "base=""$BASE" >> atualizac
 fi
 echo $linha
-read -rp "Nome de pasta da base2, Ex: sav/dados_? -:>" BASE2 
+read -rp "Nome de pasta da base2, Ex: /sav/dados_? -:>" BASE2 
 if [[ "$BASE2" == "" ]]; then
 echo "#base2=" >> atualizac
 echo "#base2="
 else
-echo "base2=/""$BASE2" >> atualizac
-echo "base2=/""$BASE2"
+echo "base2=""$BASE2" 
+echo "base2=""$BASE2" >> atualizac
 fi
 echo $linha
 
-read -rp "Nome de pasta da base3, Ex: sav/dados_? -:>" BASE3
+read -rp "Nome de pasta da base3, Ex: /sav/dados_? -:>" BASE3
 if [[ "$BASE3" == "" ]]; then
-echo "#base3=" >> atualizac
 echo "#base3="
+echo "#base3=" >> atualizac
 else
-echo "base3=/""$BASE3" >> atualizac
-echo "base3=/""$BASE3"
+echo "base3=""$BASE3"
+echo "base3=""$BASE3" >> atualizac
 fi
 echo $linha
 clear   
@@ -113,15 +113,15 @@ echo "sistema=iscobol" >> atualizac
 echo $linha
 echo "Escolha a versao do Iscobol"
 echo
-echo "1- Versao 2018"
+echo "      1- Versao 2018"
 echo
-echo "2- Versao 2020"
+echo "      2- Versao 2020"
 echo
-echo "3- Versao 2023"
+echo "      3- Versao 2023"
 echo
-echo "4- Versao 2024"
+echo "      4- Versao 2024"
 echo
-read -rp "Escolha a versão -> " -n1 VERSAO 
+read -rp "      Escolha a versão -> " -n1 VERSAO 
 echo
 case $VERSAO in
 1) _2018 ;;
@@ -208,4 +208,4 @@ case $escolha in
 		exit
 	;;
 esac 
-echo "Pronto !!!"
+echo "      Pronto !!!"
