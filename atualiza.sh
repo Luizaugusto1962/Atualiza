7f91cdef68529af271cca038cf80ac99355749ff
Luiz Augusto Rodrigues <61833154+Luizaugusto1962@users.noreply.github.com>
2025-01-28 11:31:01 -0300

    Atualiza.sh
    versão de 28/01/2025

--- atualiza.sh
+++ atualiza.sh
@@ -13,7 +13,7 @@
 ##  Rotina para atualizar os programas avulsos e bibliotecas da SAV                                                               #
 ##  Feito por: Luiz Augusto   email luizaugusto@sav.com.br                                                              #
 ##  Versao do atualiza.sh                                                                                              #
-UPDATE="27/01/2025"                                                                                                    #
+UPDATE="28/01/2025"                                                                                                    #
 #                                                                                                                      #
 #--------------------------------------------------------------------------------------------------#                   #
 # Arquivos de trabalho:                                                                                                #
